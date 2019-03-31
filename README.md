5ad1a40dcc4ca515cd60d0cb2ff0693330780dd9
Jen Bruce <thistle.mcthornbody@gmail.com>
2019-03-31 15:01:11 -0700

    Add final JS logic, README.

---
+++ README.md
@@ -0,0 +1,38 @@
+# Ping Pong Site
+
+#### _Simple pizza ordering site, fourth code review, 03.31.19_
+
+#### By **Jen Bruce**
+
+## Description
+
+_This is a minimal site to show a functioning example of object-oriented Javascript. Objects and prototypes were used to calculate the pizza's price and HTML and CSS were used to organize and style the site._
+
+## Specifications
+
+1. User selects size and price populates.
+  
+2. User selects any number of toppings and price increases appropriately.
+ 
+3. Total cost of pizza as-built by user displays when form is submitted.
+ 
+## Setup/Installation Requirements
+
+* _[Clone this repository](https://github.com/ampersnad/Pizza-Code-Review)_
+* _Open the HTML and/or Git-Pages link in your web browser_
+* _[Link to homepage](https://ampersnad.github.io/Pizza-Code-Review)_
+
+## Technologies Used
+
+* _Markdown_
+* _HTML_
+* _CSS_
+* _Bootstrap_
+* _Javascript_
+* _jQuery_
+
+### License
+
+*This software is licensed under the MIT license.*
+
+Copyright (c) 2019 **_Jen Bruce_**
--- js/scripts.js
+++ js/scripts.js
@@ -3,58 +3,52 @@
 var smallPrice = 8;
 var medPrice = 12;
 var lrgPrice = 15;
-var cheese = 1;
-var pepperoni = 1;
-var sausage = 1;
-var pineapple = 1;
-var mushroom = 1;
-var toppings = [cheese, pepperoni, sausage, pineapple, mushroom];
 
 function Order(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
 
-
 Order.prototype.calcCost = function(size, toppings) {
-	
-	
-
-  // if ((this.time === "morning" || this.time ===  "matinee") && this.age !== "youth") {
-    // return matPrice;
-  // } else {
-     // if (this.age === "youth" && this.movie === "3") {
-       // ageRestrict = "You're not old enough to view this film. BUSTED."
-     // } else if (this.age === "youth") {
-       // return youthPrice;
-     // } else {
-       // return regPrice;
-     // }
-  // }
+	return size + toppings;
 }
 
-
-//User interface logic ------------
+//User interface logic --------------------------------------
 $(document).ready(function() {
   $("form#foodOrder").submit(function(event) {
 
     event.preventDefault();
-    //$("#pizzaPrice").text("");
     
-
-    // $(".priceOutput").empty();
     //location.reload();
+	
+// Setting pricing for pizza size -----------------------------------
+    var sizeSelect = $("input:radio[name=size]:checked").val();
+    console.log("size", sizeSelect);
+	if (sizeSelect === "1") {
+		size = smallPrice;
+	};
+	if (sizeSelect === "2") {
+		size = medPrice;
+	};
+	if (sizeSelect === "3") {
+		size = lrgPrice;
+	};
+	console.log("sizeprice", size);
 
-    var size = $("input:radio[name=size]:checked").val();
-    console.log(size);
-    var toppingSelect = $("input:checkbox[name=toppings]:checked").val();
-    console.log(toppingSelect);
-    var newOrder = new Order(size, toppings);
-    //console.log(newOrder);
+// Setting pricing for toppings --------------------------------------
+	var toppingPrice = 0
+	$("input:checkbox[name=toppings]:checked").each(function(){
+			toppingPrice += 1;
+		});
+	var toppings = toppingPrice;
+	
+	
+    console.log(toppingPrice);
+    var newOrder = new Order(size, toppingPrice);
+    console.log("order", newOrder);
     var price = newOrder.calcCost(size, toppings);
+	console.log("price", price);
     $("#pizzaPrice").text(price);
-
-    
 
   });
 });
